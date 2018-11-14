b4c7eea93c0f59ebca629e23a02f5008d9cf5834
Jennifer Enck <jenniferenck@mac.com>
2018-11-14 15:57:06 -0800

    added steps 1-2

--- api-classes.js
+++ api-classes.js
@@ -1,12 +1,65 @@
 const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+let stories;
 
 class StoryList {
+  constructor(stories){
+    this.stories = stories;
+  }
+
+  // grabs the complete storylist and creates individual story instances to be access later
   static getStories(cb) {
-    // fetch stories from API
-    return cb(/* fetched stories go here */);
+    $.getJSON(`${BASE_URL}/stories`, function(response) {
+      const stories = response.stories.map(function(story) {
+        const { username, title, author, url, storyId } = story;
+        return new Story(username, title, author, url, storyId);
+      });
+      const storyList = new StoryList(stories);
+      return cb(storyList);
+    });
   }
 }
 
-class User {}
+class User {
+  constructor(username, password, name, loginToken, favorites = [], ownStories){
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.loginToken = loginToken;
+    this.favorites = favorites;
+    this.ownStories = ownStories;
+  }
 
-class Story {}
+  static create(username, password, name, callback){
+    $.post(`${BASE_URL}/signup`, 
+    { "user":{"name":name,"username":username,"password":password} },
+    function(response){
+      console.log(response);
+      const { token, favorites, ownStories } = response; // data request from the API
+      const user = new User(username, password, name, token, favorites, ownStories); // creating a new instance from the aggregate data from API and new user
+      return callback(user);
+    });
+
+  }
+}
+
+
+// constructor of each story instance from the API
+class Story {
+  constructor(author, title, url, username, storyId){
+    this.author = author; 
+    this.title = title; 
+    this.url = url; 
+    this.username = username; 
+    this.storyId = storyId;
+  }
+}
+
+// invoking static function 
+StoryList.getStories(function(response){
+  stories = response;
+});
+
+let user;
+User.create("ChaCha", "hey", "Whiskey", function(response){
+  user = response;
+});
