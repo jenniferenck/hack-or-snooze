e2ef9484914af3f430c7c1b3a4145dcf4564140b
Chad Schroeder <gmchad@gmail.com>
2018-11-15 14:45:13 -0800

    update method for Story class

--- api-classes.js
+++ api-classes.js
@@ -18,8 +18,16 @@ class StoryList {
     });
   }
 
-  // parameters:
   addStory(user, entryData, callback) {
+    /*
+    Story {
+      author: "testing6667"
+      storyId: "d661e762-962f-43f4-93d9-c511b9b83692"
+      title: "testing again"
+      url: "A Rithm Instructor"
+      username: "https://www.rithmschool.com"
+    }
+    */
     $.post(
       `${BASE_URL}/stories`,
       { token: user.loginToken, story: entryData },
@@ -80,6 +88,35 @@ class User {
     );
   }
 
+  addFavorite(storyId, callback) {
+    $.ajax({
+      url: `${BASE_URL}/users/${user.username}/favorites/${storyId}`,
+      method: 'POST',
+      data: {
+        token: user.loginToken
+      },
+      success: () => {
+        user.retrieveDetails(() => callback(this));
+      }
+    });
+  }
+
+  removeFavorite(storyId, callback) {
+    $.ajax({
+      url: `${BASE_URL}/users/${user.username}/favorites/${storyId}`,
+      method: 'DELETE',
+      data: {
+        token: user.loginToken
+      },
+      success: () => {
+        user.retrieveDetails(() => callback(this));
+      },
+      error: response => {
+        console.log(response);
+      }
+    });
+  }
+
   login(callback) {
     $.post(
       `${BASE_URL}/login`,
@@ -102,8 +139,24 @@ class User {
       `${BASE_URL}/users/${this.username}?token=${this.loginToken}`,
       response => {
         this.name = response.user.name;
-        this.favorites = response.user.favorites;
-        this.ownStories = response.user.stories;
+        this.favorites = response.user.favorites.map(story => {
+          return new Story(
+            story.author,
+            story.title,
+            story.url,
+            story.username,
+            story.storyId
+          );
+        });
+        this.ownStories = response.user.stories.map(story => {
+          return new Story(
+            story.author,
+            story.title,
+            story.url,
+            story.username,
+            story.storyId
+          );
+        });
         return callback(this);
       }
     );
@@ -118,6 +171,24 @@ class Story {
     this.url = url;
     this.username = username;
     this.storyId = storyId;
+  }
+
+  update(user, storyData, callback) {
+    $.ajax({
+      url: `${BASE_URL}/stories/${this.storyId}`,
+      method: 'PATCH',
+      data: {
+        token: user.loginToken,
+        story: storyData
+      },
+      success: response => {
+        const { author, title, url } = response.story;
+        this.author = author;
+        this.title = title;
+        this.url = url;
+        return callback(this);
+      }
+    });
   }
 }
 
@@ -148,13 +219,43 @@ User.create(
         };
         storyList.addStory(user, newStoryData, function(response) {
           // should be array of all stories including new story
-          console.log('Created a story: ', response);
-
           var firstStory = user.ownStories[0];
-          console.log('firstStory: ', firstStory.storyId);
-
           storyList.removeStory(user, firstStory.storyId, function(response) {
-            console.log('Deleted a story:', response);
+            // console.log('Deleted a story:', response);
+          });
+
+          var newStoryData = {
+            title: 'testing again',
+            author: 'A Rithm Instructor',
+            url: 'https://www.rithmschool.com'
+          };
+
+          storyList.addStory(user, newStoryData, function(response) {
+            var firstStory = user.ownStories[0];
+            user.addFavorite(firstStory.storyId, function(response) {
+              console.log(firstStory.storyId, response); // this should include the added favorite!
+            });
+          });
+
+          storyList.addStory(user, newStoryData, function(response) {
+            var firstStory = user.ownStories[0];
+            user.addFavorite(firstStory.storyId, function(response) {
+              user.removeFavorite(firstStory.storyId, function(response) {
+                // this should include the removed favorite!
+                console.log(response);
+                var updatedData = {
+                  title: 'NO MORE TESTING!',
+                  author: 'A Rithm Instructor',
+                  url: 'https://www.taco.com'
+                };
+                console.log('User story:', user.ownStories[0]);
+                user.ownStories[0].update(user, updatedData, function(
+                  response
+                ) {
+                  console.log(response); // this should be the updated story instance!
+                });
+              });
+            });
           });
         });
       });
