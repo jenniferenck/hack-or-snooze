01ca952383743db5eec73ed12de98b7a0e8a0411
Chad Schroeder <gmchad@gmail.com>
2018-11-15 11:49:26 -0800

    merged our classes with solutions

--- api-classes.js
+++ api-classes.js
@@ -1,8 +1,8 @@
-const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+const BASE_URL = 'https://hack-or-snooze-v2.herokuapp.com';
 let storyList;
 
 class StoryList {
-  constructor(stories){
+  constructor(stories) {
     this.stories = stories;
   }
 
@@ -18,80 +18,94 @@ class StoryList {
     });
   }
 
-  // parameters: 
-  addStory(user, entryData, callback){
-    $.post(`${BASE_URL}/stories`, 
-    {"token": user.loginToken, "story": entryData }, 
-    function(response){
-      user.retrieveDetails(function(response){
-        console.log(response);
-        user.ownStories.push(response.user.stories[0]);// for now, assuming only one item in the array
-      })
-    });
+  // parameters:
+  addStory(user, entryData, callback) {
+    $.post(
+      `${BASE_URL}/stories`,
+      { token: user.loginToken, story: entryData },
+      response => {
+        const { author, title, url, username, storyId } = response.story;
+        const newStory = new Story(author, title, url, username, storyId);
+        this.stories.push(newStory);
+        user.retrieveDetails(() => callback(newStory));
+      }
+    );
   }
 }
 
 class User {
-  constructor(username, password, name, loginToken = "", favorites = [], ownStories = []){
+  constructor(username, password, name, loginToken, favorites, ownStories) {
     this.username = username;
     this.password = password;
     this.name = name;
     this.loginToken = loginToken;
-    this.favorites = favorites;
-    this.ownStories = ownStories;
+    this.favorites = [];
+    this.ownStories = [];
   }
 
-  static create(username, password, name, callback){
-    $.post(`${BASE_URL}/signup`, 
-    { "user":{"name":name,"username":username,"password":password} },
-    function(response){
-      const { token } = response; // data request from the API
-      const user = new User(username, password, name, token); // creating a new instance from the aggregate data from API and new user
-      return callback(user);
-    });
-  }
-
-  login(callback){
+  static create(username, password, name, callback) {
     $.post(
-      `${BASE_URL}/login`, 
+      `${BASE_URL}/signup`,
       {
-        "user": {
-            "username": this.username,
-            "password": this.password
+        user: {
+          username,
+          password,
+          name
         }
       },
-      function(response){
-        console.log(response);
-        this.loginToken = response.token;
-        return callback(response);
+      function(response) {
+        const { token } = response;
+        const { username, name } = response.user;
+        // localStorage goes here
+        const newUser = new User(username, password, name, token);
+        return callback(newUser);
       }
-    )
+    );
   }
 
-  retrieveDetails(callback){
-    $.get(`${BASE_URL}/users/${this.username}?token=${this.loginToken}`,
-    function(response){
-      this.favorites = response.favorites;
-      this.ownStories =response.ownStories;
-      return callback(response);
-    })
+  login(callback) {
+    $.post(
+      `${BASE_URL}/login`,
+      {
+        user: {
+          username: this.username,
+          password: this.password
+        }
+      },
+      response => {
+        this.loginToken = response.token;
+        // localStorage goes here
+        return callback(response);
+      }
+    );
+  }
+
+  retrieveDetails(callback) {
+    $.get(
+      `${BASE_URL}/users/${this.username}?token=${this.loginToken}`,
+      response => {
+        this.name = response.user.name;
+        this.favorites = response.user.favorites;
+        this.ownStories = response.user.ownStories;
+        return callback(this);
+      }
+    );
   }
 }
 
-
 // constructor of each story instance from the API
 class Story {
-  constructor(author, title, url, username, storyId){
-    this.author = author; 
-    this.title = title; 
-    this.url = url; 
-    this.username = username; 
+  constructor(author, title, url, username, storyId) {
+    this.author = author;
+    this.title = title;
+    this.url = url;
+    this.username = username;
     this.storyId = storyId;
   }
 }
 
-// invoking static function 
-StoryList.getStories(function(response){
+// invoking static function
+StoryList.getStories(function(response) {
   storyList = response;
 });
 
@@ -100,30 +114,30 @@ User.create(
   `testing${Math.floor(Math.random() * 10000)}`,
   `testing${Math.floor(Math.random() * 10000)}`,
   `testing${Math.floor(Math.random() * 10000)}`,
-  
-  function (newUser) {
+
+  function(newUser) {
     // this should be object containing newly created user
     user = newUser;
     // console.log(user);
     // using the `user` variable from above:
-    user.login(function (data) {
+    user.login(function(data) {
       // should be object containing user info along with loginToken
       // console.log(data);
-      user.retrieveDetails(function (response){
-      // console.log(response) // this should be the user
+      user.retrieveDetails(function(response) {
+        // console.log(response) // this should be the user
         // using the `user` and `storyList` variables from above:
-        var newStoryData = {title: "testing again",
-        author: "A Rithm Instructor",
-        url: "https://www.rithmschool.com"};
-        storyList.addStory(user, newStoryData, function (response) {
-        // should be array of all stories including new story
-        console.log(response);
-        // should be array of all stories written by user
-        console.log(user.stories);
-        })
+        var newStoryData = {
+          title: 'testing again',
+          author: 'A Rithm Instructor',
+          url: 'https://www.rithmschool.com'
+        };
+        storyList.addStory(user, newStoryData, function(response) {
+          // should be array of all stories including new story
+          console.log(response);
+          // should be array of all stories written by user
+          // console.log(user.stories);
+        });
       });
     });
   }
 );
-
-
